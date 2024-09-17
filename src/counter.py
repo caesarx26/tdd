65f0830aae8fc970af4aa27d480b1aae16e82285
caesarx26 <caesarrobles26@gmail.com>
2024-09-17 00:31:44 -0700

    adding tests and implementing endpoints

---
+++ src/counter.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from . import status
+
+app = Flask(__name__)
+COUNTERS = {}
+
+# We will use the app decorator and create a route called slash counters.
+# specify the variable in route <name>
+# let Flask know that the only methods that is allowed to called
+# on this function is "POST".
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    """update counter by 1"""
+    app.logger.info(f"Request to update counter by 1: {name}")
+    global COUNTERS
+    if name not in COUNTERS:
+        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+    COUNTERS[name] += 1
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
