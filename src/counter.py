3e0dde83c95d322c86808b7079027f0e3d8064d6
caesarx26 <caesarrobles26@gmail.com>
2024-09-22 20:31:18 -0700

    updating python counter file for linting

--- src/counter.py
+++ src/counter.py
@@ -4,9 +4,10 @@ from . import status
 app = Flask(__name__)
 COUNTERS = {}
 
+
 # We will use the app decorator and create a route called slash counters.
 # specify the variable in route <name>
-# let Flask know that the only methods that is allowed to called
+# let Flask know that the only methods that are allowed to be called
 # on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name): 
@@ -14,16 +15,17 @@ def create_counter(name):
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
+
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name): 
-    """update counter by 1"""
+    """Update counter by 1"""
     app.logger.info(f"Request to update counter by 1: {name}")
     global COUNTERS
     if name not in COUNTERS:
-        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
